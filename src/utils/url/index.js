8861500d17f63a1ee21cdf831c3d3f627a7648ef
farish-jamal <farishjamal98@gmail.com>
2026-02-07 10:24:40 +0530

    fix: Update BACKEND_URL to new deployment URL for improved access

--- src/utils/url/index.js
+++ src/utils/url/index.js
@@ -1,4 +1,5 @@
-export const BACKEND_URL = "https://theceliacstore-be.onrender.com";
+// export const BACKEND_URL = "https://theceliacstore-be.onrender.com";
 // export const BACKEND_URL = "http://localhost:5000";
 // export const SHIPROCKET_URL = "https://apiv2.shiprocket.in";
 // Mirror Shiprocket URL for faster access
+export const BACKEND_URL = "https://theceliacstore-be-7as7.onrender.com";
